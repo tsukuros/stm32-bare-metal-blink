cf3ec9fb3001e7f74aae082af796341942e3fc94
Oleksandr Tsukur <tsukuros@gmail.com>
2026-04-01 01:49:42 +0300

    feat: add register-level blink code and build toolchain

--- src/main.c
+++ src/main.c
@@ -1,4 +1,12 @@
+#include "stm32f446xx.h"
+
 int main() {
+  RCC->AHB1ENR |= (1 << 0); // enable GPIOA clock
+  GPIOA->MODER &= ~(0x3 << 10);
+  GPIOA->MODER |= (0x1 << 10);
   while (1) {
+    GPIOA->ODR ^= (1 << 5);
+    for (volatile uint32_t i = 0; i < 500000; i++)
+      ;
   }
 }
